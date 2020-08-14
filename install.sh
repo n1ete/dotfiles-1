dfd07aa1d740bb38109d3e14b90d85bdce2459b5
jcccb <schadensregulierung@gmail.com>
2020-08-14 20:21:39 +0200

    remove needed package install (not worth it)

--- install.sh
+++ install.sh
@@ -19,8 +19,6 @@
 #
 # - Connect to wifi via: `# wifi-menu`
 # - Run: `# bash <(curl -sL https://git.io/maximbaz-install)`
-pacman -Sy
-pacman -S git wget dialog
 
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
